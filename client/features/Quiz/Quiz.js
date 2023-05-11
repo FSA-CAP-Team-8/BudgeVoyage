8c6515c9833c1aef9bf36fb1ca0e50dbf2a9d4bc
veronica cisneors <vcisneroshdz@gmail.com>
2023-05-11 08:01:27 -0700

    changes to css and quiz

--- client/features/Quiz/Quiz.js
+++ client/features/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 
@@ -6,10 +6,23 @@ const Quiz = () => {
   const dispatch = useDispatch();
   const quiz = useSelector((state) => state.quiz);
 
+  const setOfQuestions = [
+    {
+      questionText: "What is your budget?",
+      answerOption: [{ answerText: "blahblah" }],
+    },
+    {
+      questionText: "Where are you traveling?",
+      answerOption: [{ answerText: "blahblah" }],
+    },
+  ];
+
+  const handleSubmit = (quiz) => {};
+
   return (
-    <>
-      <div id="quiz">
-        <h1>What is your budget?</h1>
+    <div id="quiz">
+      <div className="container">
+        {/* <h1>{setOfQuestions[0]}</h1> */}
         <h1>Where are you traveling to?</h1>
         <h1>Which airline are you traveling with?</h1>
         <h1>Which days will you need lodging?</h1>
@@ -17,12 +30,15 @@ const Quiz = () => {
         <h1>How many adults and children are you traveling with?</h1>
         <h1>Are you traveling with a pet?</h1>
         <div>
-          <button onClick={() => handleSubmit(quiz)}>
-            READY FOR MY VOYAGE
+          <button id="quizbtn" onClick={() => handleSubmit(quiz)}>
+            Ready for Your Voyage
+          </button>
+          <button id="nextbtn" onClick={() => handleSubmit(quiz)}>
+            next
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
