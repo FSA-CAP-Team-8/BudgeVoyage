7a3eb4d4cc5e5f5ac6d8105e3fee8e5d9f345575
jesiah <jesiah.roe@gmail.com>
2023-05-11 17:40:14 -0400

    flight card and lodging card

--- client/features/Card/Card.js
+++ client/features/Card/Card.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import Airbnb from "../Airbnb/Airbnb";
+import Hotel from "../Hotels/Hotel";
+import Flights from "../Flights/Flights";
+const lodging = (Airbnb, Hotel);
+const flight = Flights;
 
 const Card = () => {
   const dispatch = useDispatch();
@@ -8,7 +13,15 @@ const Card = () => {
 
   return (
     <>
-      <div id="card"></div>
+      <div id="flightcard">
+        <image id=""></image>
+        <div id="flightprice">${flight.price}</div>
+      </div>
+
+      <div id="lodgingcard">
+        <image id=""></image>
+        <div id="lodgingprice">${lodging.price}</div>
+      </div>
     </>
   );
 };
