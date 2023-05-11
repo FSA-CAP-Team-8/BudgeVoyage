beed8c3a52f3541266af3778e5478136fe9d78be
Luis Samboy <luissamboy@icloud.com>
2023-05-11 12:14:28 -0700

    Commented out dummy data

--- client/features/Airbnb/Airbnb.js
+++ client/features/Airbnb/Airbnb.js
@@ -1,174 +1,174 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchAirbnbListings } from "./airSlice";
-
-// const Airbnb = ({ destination, checkin, checkout, adults }) => {
-//   const dispatch = useDispatch();
-//   const airbnb = useSelector((state) => state.airbnb);
-
-//   useEffect(() => {
-//     dispatch(
-//       fetchAirbnbListings({
-//         destination: destination,
-//         checkin: checkin,
-//         checkout: checkout,
-//         adults: adults,
-//       })
-//     );
-//   }, [dispatch, destination, checkin, checkout, adults]);
-
-//   return (
-//     <div className="component">
-//       <h2>Airbnb</h2>
-//       {airbnb.results && (
-//         <div className="apiResults">
-//           {airbnb.results.map((result, index) => (
-//             <div key={`result-${index}`}>
-//               <div className="resultsCards">
-//                 <img
-//                   src={result.images[0]}
-//                   alt="airbnbImages"
-//                   style={{ width: "300px", height: "225px" }}
-//                 />
-//                 <a
-//                   href={result.deeplink}
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                 >
-//                   {" "}
-//                   <div>{result.name}</div>
-//                 </a>
-//                 <li>Type: {result.type}</li>{" "}
-//                 <li>Total Price: ${result.price.total}</li>{" "}
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Airbnb;
-
-/** The Following Data is Dummy Data */
-
 import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAirbnbListings } from "./airSlice";
 
-const Airbnb = () => {
-  const dummyData = [
-    {
-      name: "Airbnb Listing A",
-      images: ["https://example.com/airbnb-a.jpg"],
-      deeplink: "https://example.com/airbnb-a",
-      type: "Entire Home",
-      price: {
-        total: 200,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-    {
-      name: "Airbnb Listing B",
-      images: ["https://example.com/airbnb-b.jpg"],
-      deeplink: "https://example.com/airbnb-b",
-      type: "Private Room",
-      price: {
-        total: 100,
-      },
-    },
-  ];
+const Airbnb = ({ destination, checkin, checkout, adults }) => {
+  const dispatch = useDispatch();
+  const airbnb = useSelector((state) => state.airbnb);
+
+  useEffect(() => {
+    dispatch(
+      fetchAirbnbListings({
+        destination: destination,
+        checkin: checkin,
+        checkout: checkout,
+        adults: adults,
+      })
+    );
+  }, [dispatch, destination, checkin, checkout, adults]);
 
   return (
     <div className="component">
       <h2>Airbnb</h2>
-      <div className="apiResults">
-        {dummyData.map((result, index) => (
-          <div key={`result-${index}`}>
-            <div className="resultsCards">
-              <img
-                src={result.images[0]}
-                alt="airbnbImages"
-                style={{ width: "300px", height: "225px" }}
-              />
-              <a
-                href={result.deeplink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {" "}
-                <div>{result.name}</div>
-              </a>
-              <li>Type: {result.type}</li>{" "}
-              <li>Total Price: ${result.price.total}</li>{" "}
+      {airbnb.results && (
+        <div className="apiResults">
+          {airbnb.results.map((result, index) => (
+            <div key={`result-${index}`}>
+              <div className="resultsCards">
+                <img
+                  src={result.images[0]}
+                  alt="airbnbImages"
+                  style={{ width: "300px", height: "225px" }}
+                />
+                <a
+                  href={result.deeplink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  <div>{result.name}</div>
+                </a>
+                <li>Type: {result.type}</li>{" "}
+                <li>Total Price: ${result.price.total}</li>{" "}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 export default Airbnb;
+
+/** The Following Data is Dummy Data */
+
+// import React from "react";
+
+// const Airbnb = () => {
+//   const dummyData = [
+//     {
+//       name: "Airbnb Listing A",
+//       images: ["https://example.com/airbnb-a.jpg"],
+//       deeplink: "https://example.com/airbnb-a",
+//       type: "Entire Home",
+//       price: {
+//         total: 200,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//     {
+//       name: "Airbnb Listing B",
+//       images: ["https://example.com/airbnb-b.jpg"],
+//       deeplink: "https://example.com/airbnb-b",
+//       type: "Private Room",
+//       price: {
+//         total: 100,
+//       },
+//     },
+//   ];
+
+//   return (
+//     <div className="component">
+//       <h2>Airbnb</h2>
+//       <div className="apiResults">
+//         {dummyData.map((result, index) => (
+//           <div key={`result-${index}`}>
+//             <div className="resultsCards">
+//               <img
+//                 src={result.images[0]}
+//                 alt="airbnbImages"
+//                 style={{ width: "300px", height: "225px" }}
+//               />
+//               <a
+//                 href={result.deeplink}
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 {" "}
+//                 <div>{result.name}</div>
+//               </a>
+//               <li>Type: {result.type}</li>{" "}
+//               <li>Total Price: ${result.price.total}</li>{" "}
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Airbnb;
--- client/features/Hotels/Hotel.js
+++ client/features/Hotels/Hotel.js
@@ -1,259 +1,259 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchHotelID, fetchHotelPrices } from "./hotelIDSlice";
-
-// const Hotel = ({
-//   destination,
-//   checkin,
-//   checkout,
-//   adults,
-//   children,
-//   childAges,
-// }) => {
-//   const dispatch = useDispatch();
-//   const hotelID = useSelector((state) => state.hotelID.data);
-//   const error = useSelector((state) => state.hotelID.error);
-
-//   useEffect(() => {
-//     dispatch(
-//       fetchHotelID({
-//         destination: destination,
-//       })
-//     );
-//   }, [dispatch, destination]);
-
-//   useEffect(() => {
-//     if (hotelID) {
-//       hotelID.forEach((hotel) => {
-//         dispatch(
-//           fetchHotelPrices({
-//             dest_id: hotel.dest_id,
-//             checkin: checkin,
-//             checkout: checkout,
-//             adults: adults,
-//             children: children,
-//             childAges: childAges,
-//           })
-//         );
-//       });
-//     }
-//   }, [hotelID, dispatch, checkin, checkout, adults, childAges]);
-
-//   return (
-//     <div className="component">
-//       <h2>Hotels</h2>
-//       {error && <p>Error: {error}</p>}
-//       {hotelID && (
-//         <div className="apiResults">
-//           {hotelID.map((result) => (
-//             <div key={result.dest_id}>
-//               <div>
-//                 {result.prices && result.prices.results && (
-//                   <div>
-//                     {result.prices.results.map((priceResult) => (
-//                       <div key={priceResult.id}>
-//                         <div className="resultsCards">
-//                           <img
-//                             src={priceResult.photoMainUrl}
-//                             alt="hotelImages"
-//                             style={{ width: "300px", height: "225px" }}
-//                           />
-//                           <li>Hotel Name: {priceResult.name}</li>
-//                           <li>
-//                             Total Price: $
-//                             {priceResult.priceBreakdown.grossPrice.value}
-//                           </li>
-//                         </div>
-//                       </div>
-//                     ))}
-//                   </div>
-//                 )}
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Hotel;
-
-/** The following is dummy data */
-
 import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchHotelID, fetchHotelPrices } from "./hotelIDSlice";
 
-const Hotel = () => {
-  const dummyData = [
-    {
-      dest_id: 1,
-      prices: {
-        results: [
-          {
-            id: 1,
-            name: "Hotel A",
-            photoMainUrl: "https://example.com/hotel-a.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 100,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 2,
-            name: "Hotel B",
-            photoMainUrl: "https://example.com/hotel-b.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 150,
-                currency: "USD",
-              },
-            },
-          },
-        ],
-      },
-    },
-    {
-      dest_id: 2,
-      prices: {
-        results: [
-          {
-            id: 3,
-            name: "Hotel C",
-            photoMainUrl: "https://example.com/hotel-c.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 120,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-          {
-            id: 4,
-            name: "Hotel D",
-            photoMainUrl: "https://example.com/hotel-d.jpg",
-            priceBreakdown: {
-              grossPrice: {
-                value: 200,
-                currency: "USD",
-              },
-            },
-          },
-        ],
-      },
-    },
-  ];
+const Hotel = ({
+  destination,
+  checkin,
+  checkout,
+  adults,
+  children,
+  childAges,
+}) => {
+  const dispatch = useDispatch();
+  const hotelID = useSelector((state) => state.hotelID.data);
+  const error = useSelector((state) => state.hotelID.error);
+
+  useEffect(() => {
+    dispatch(
+      fetchHotelID({
+        destination: destination,
+      })
+    );
+  }, [dispatch, destination]);
+
+  useEffect(() => {
+    if (hotelID) {
+      hotelID.forEach((hotel) => {
+        dispatch(
+          fetchHotelPrices({
+            dest_id: hotel.dest_id,
+            checkin: checkin,
+            checkout: checkout,
+            adults: adults,
+            children: children,
+            childAges: childAges,
+          })
+        );
+      });
+    }
+  }, [hotelID, dispatch, checkin, checkout, adults, childAges]);
 
   return (
     <div className="component">
       <h2>Hotels</h2>
-      <div className="apiResults">
-        {dummyData.map((result) => (
-          <div key={result.dest_id}>
-            {result.prices && result.prices.results && (
+      {error && <p>Error: {error}</p>}
+      {hotelID && (
+        <div className="apiResults">
+          {hotelID.map((result) => (
+            <div key={result.dest_id}>
               <div>
-                {result.prices.results.map((priceResult) => (
-                  <div key={priceResult.id}>
-                    <div className="resultsCards">
-                      <img
-                        src={priceResult.photoMainUrl}
-                        alt="hotelImages"
-                        style={{ width: "300px", height: "225px" }}
-                      />
-                      <li>Hotel Name: {priceResult.name}</li>
-                      <li>
-                        Total Price: $
-                        {priceResult.priceBreakdown.grossPrice.value}
-                      </li>
-                    </div>
+                {result.prices && result.prices.results && (
+                  <div>
+                    {result.prices.results.map((priceResult) => (
+                      <div key={priceResult.id}>
+                        <div className="resultsCards">
+                          <img
+                            src={priceResult.photoMainUrl}
+                            alt="hotelImages"
+                            style={{ width: "300px", height: "225px" }}
+                          />
+                          <li>Hotel Name: {priceResult.name}</li>
+                          <li>
+                            Total Price: $
+                            {priceResult.priceBreakdown.grossPrice.value}
+                          </li>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                )}
               </div>
-            )}
-          </div>
-        ))}
-      </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 export default Hotel;
+
+/** The following is dummy data */
+
+// import React from "react";
+
+// const Hotel = () => {
+//   const dummyData = [
+//     {
+//       dest_id: 1,
+//       prices: {
+//         results: [
+//           {
+//             id: 1,
+//             name: "Hotel A",
+//             photoMainUrl: "https://example.com/hotel-a.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 100,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 2,
+//             name: "Hotel B",
+//             photoMainUrl: "https://example.com/hotel-b.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 150,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//         ],
+//       },
+//     },
+//     {
+//       dest_id: 2,
+//       prices: {
+//         results: [
+//           {
+//             id: 3,
+//             name: "Hotel C",
+//             photoMainUrl: "https://example.com/hotel-c.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 120,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//           {
+//             id: 4,
+//             name: "Hotel D",
+//             photoMainUrl: "https://example.com/hotel-d.jpg",
+//             priceBreakdown: {
+//               grossPrice: {
+//                 value: 200,
+//                 currency: "USD",
+//               },
+//             },
+//           },
+//         ],
+//       },
+//     },
+//   ];
+
+//   return (
+//     <div className="component">
+//       <h2>Hotels</h2>
+//       <div className="apiResults">
+//         {dummyData.map((result) => (
+//           <div key={result.dest_id}>
+//             {result.prices && result.prices.results && (
+//               <div>
+//                 {result.prices.results.map((priceResult) => (
+//                   <div key={priceResult.id}>
+//                     <div className="resultsCards">
+//                       <img
+//                         src={priceResult.photoMainUrl}
+//                         alt="hotelImages"
+//                         style={{ width: "300px", height: "225px" }}
+//                       />
+//                       <li>Hotel Name: {priceResult.name}</li>
+//                       <li>
+//                         Total Price: $
+//                         {priceResult.priceBreakdown.grossPrice.value}
+//                       </li>
+//                     </div>
+//                   </div>
+//                 ))}
+//               </div>
+//             )}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hotel;
