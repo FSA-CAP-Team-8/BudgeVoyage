1fbe394539ad0fe23a2e646996a927adb3d55188
veronica cisneors <vcisneroshdz@gmail.com>
2023-05-11 14:05:28 -0700

    creating quiz, rendering one question at a time

--- client/features/Quiz/Quiz.js
+++ client/features/Quiz/Quiz.js
@@ -5,104 +5,104 @@ import Airbnb from "../Airbnb/Airbnb";
 import Hotel from "../Hotels/Hotel";
 
 const Quiz = () => {
-  const dispatch = useDispatch();
-  const quiz = useSelector((state) => state.quiz);
-
   const [destination, setDestination] = useState("");
   const [checkin, setCheckin] = useState("");
   const [checkout, setCheckout] = useState("");
   const [adults, setAdults] = useState(1);
   const [children, setChildren] = useState(0);
   const [childAges, setChildAges] = useState([]);
+
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState({
+    budget: "",
+    travelFrom: "",
+    travelTo: "",
+  });
+  const [showBudgeCard, setShowBudgeCard] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
-    setDestination(event.target.value);
-  };
-
-  const handleCheckinChange = (event) => {
-    setCheckin(event.target.value);
-  };
-
-  const handleCheckoutChange = (event) => {
-    setCheckout(event.target.value);
-  };
-
-  const handleAdultsChange = (event) => {
-    setAdults(event.target.value);
-  };
-
-  const handleChildrenChange = (event) => {
-    setChildren(event.target.value);
-  };
-
-  const handleChildAgeChange = (index, event) => {
-    const newChildAges = [...childAges];
-    newChildAges[index] = event.target.value;
-    setChildAges(newChildAges);
+    const { name, value } = event.target;
+    setAnswers({ ...answers, [name]: value });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     setIsSubmitted(true);
   };
 
   const setOfQuestions = [
     {
       questionText: "What is your budget?",
-      answerOption: { answertest: "New York" },
+      answerOption: { destination },
     },
     {
-      questionText: "Where are you traveling from and to ?",
+      questionText: "Where are you traveling from and to?",
       answerOption: { answertest: "New York" },
     },
-    // {
-    //   questionText: "How many adults and children are you traveling with?",
-    //   answerOption: { value: children, value: adults },
-    // },
-    // {
-    //   questionText: "what airlines do you prefer?",
-    //   answerOption: { value: destination },
-    // },
-    // {
-    //   questionText: "What days are you staying?",
-    //   answerOption: { value: checkin, value: checkout },
-    // },
-    // {
-    //   questionText: "What is your lodging preference?",
-    //   answerOption: { value: destination },
-    // },
-    // {
-    //   questionText: "How many rooms or bed do you need?",
-    //   answerOption: { value: destination },
-    // },
   ];
 
   return (
-    <div id="quiz">
-      <div className="container">
-        <h1>{setOfQuestions[0]}</h1>
-        <h1>{setOfQuestions[1]}</h1>
-        {/* <h1>{setOfQuestions[2]}</h1>
-        <h1>{setOfQuestions[3]}</h1>
-        <h1>{setOfQuestions[4]}</h1>
-        <h1>{setOfQuestions[5]}</h1> */}
-        <div>
-          <div id="submitbtn">
-            {!setOfQuestions[5] ? (
-              <button id="nextbtn" onClick={() => handleSubmit(quiz)}>
-                next
+    <form onSubmit={handleSubmit}>
+      <div id="quiz">
+        <div className="container">
+          <h1>{setOfQuestions[0].questionText}</h1>
+          <input
+            type="text"
+            name="budget"
+            value={answers.budget}
+            onChange={handleInputChange}
+          />
+
+          <h1>{setOfQuestions[1].questionText}</h1>
+          <input
+            type="text"
+            name="travelFrom"
+            value={answers.travelFrom}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="travelTo"
+            value={answers.travelTo}
+            onChange={handleInputChange}
+          />
+          <div>
+            <div id="submitbtn">
+              <button id="nextbtn" type="submit">
+                {isSubmitted ? "Ready for you Voyage" : "Next"}
               </button>
-            ) : (
-              <button id="quizbtn" onClick={() => handleSubmit(quiz)}>
-                Ready for Your Voyage
-              </button>
-            )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
 export default Quiz;
+
+// {
+//   isSubmitted && (
+//     <>
+//       <Hotel
+//         destination={destination}
+//         checkin={checkin}
+//         checkout={checkout}
+//         adults={adults}
+//         children={children}
+//         childAges={childAges}
+//       />
+//       <Airbnb
+//         destination={destination}
+//         checkin={checkin}
+//         checkout={checkout}
+//         adults={adults}
+//         children={children}
+//         childAges={childAges}
+//       />
+//     </>
+//   );
+// }
