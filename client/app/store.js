3e255ed68047699628953bda844f76616ee733d6
jackie chung <jackiechung6@gmail.com>
2023-05-12 18:06:40 -0400

    imported reducer to appstore for auth

--- client/app/store.js
+++ client/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import airbnbReducer from "../features/Airbnb/airSlice";
 import hotelIDReducer from "../features/Hotels/hotelIDSlice";
+import authReducer from "../features/Authform/authSlice";
 
 const store = configureStore({
   reducer: {
     airbnb: airbnbReducer,
     hotelID: hotelIDReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
