d91e051d20a6b688a121c7f3aad5fe93c73bd3fd
veronica cisneors <vcisneroshdz@gmail.com>
2023-05-11 23:12:22 -0700

    adding profile of team

--- client/features/OurTeam.js
+++ client/features/OurTeam.js
@@ -8,11 +8,11 @@ const OurTeam = () => {
         <p>
           Hello, we are the team behind BudgeVoyage, an innovative application
           to help you travel within your budget. We all had one thing in common,
-          we love to travel but hate the planning process. We wanted to create
+          we love to travel, but hate the planning process. We wanted to create
           an app that worked as a travel advisors and focused on our users
           budget. Our application allows users to focus on the fun and leave the
           financial calculations to BudgeVoyage. Take our quiz and enjoy the
-          stress free experience of finding the right travel package fr your
+          stress free experience of finding the right travel package for your
           financial needs.
         </p>
         <div className="teamrow">
@@ -25,7 +25,58 @@ const OurTeam = () => {
                 <p>some text that descries me lorem ipsum ipsum lorem.</p>
                 <p>vcisneroshdz@gmail.com</p>
                 <p>
-                  <button className="btn">contact</button>
+                  <button className="teambtn">contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="teamrow">
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Luis"></img>
+              <div class="teamcontainer">
+                <h2>Luis Samboy</h2>
+                <p className="title">add role here</p>
+                <p>some text that descries me lorem ipsum ipsum lorem.</p>
+                <p>vcisneroshdz@gmail.com</p>
+                <p>
+                  <button className="teambtn">contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="teamrow">
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Jesia"></img>
+              <div class="teamcontainer">
+                <h2>Jesiah Roe</h2>
+                <p className="title">add role here</p>
+                <p>some text that descries me lorem ipsum ipsum lorem.</p>
+                <p>vcisneroshdz@gmail.com</p>
+                <p>
+                  <button className="teambtn">contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="teamrow">
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Jackie"></img>
+              <div class="teamcontainer">
+                <h2>Jackie Chung</h2>
+                <p className="title">add role here</p>
+                <p>some text that descries me lorem ipsum ipsum lorem.</p>
+                <p>vcisneroshdz@gmail.com</p>
+                <p>
+                  <button className="teambtn">contact</button>
                 </p>
               </div>
             </div>
