22d46c5eaad23b193242a41b056bb0ef444d1716
Luis Samboy <luissamboy@icloud.com>
2023-05-11 17:41:58 -0700

    Final styling commit before review

--- client/features/Navbar.js
+++ client/features/Navbar.js
@@ -6,21 +6,17 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <div>
-        <img className="logo" src="budgeVoyage2.png"></img>
-        <nav>
-          <div>
-            <NavLink to="/">BudgeVoyage</NavLink>
-            <NavLink to="/ourstory">Our Story</NavLink>
-            <NavLink to="/search">Search</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
-            <NavLink to="/bucketlist">
-              <img className="bucketlogo" src="bucketListlogo.png"></img>
-            </NavLink>
-          </div>
-        </nav>
-      </div>
+      <img className="logo" src="budgeVoyage2.png"></img>
+      <nav>
+        <NavLink to="/">BudgeVoyage</NavLink>
+        <NavLink to="/ourstory">Our Story</NavLink>
+        <NavLink to="/search">Search</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/bucketlist">
+          <img className="bucketlogo" src="bucketListlogo.png"></img>
+        </NavLink>
+      </nav>
     </header>
   );
 };
