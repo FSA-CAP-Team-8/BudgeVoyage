98b58f3da29ee5a4dcf0b4a51765a4ad0898dcd0
jackie chung <jackiechung6@gmail.com>
2023-05-15 15:14:46 -0400

    set up const and changed routes for seed file

---
+++ seed.js
@@ -0,0 +1,5 @@
+const db = require("./server/db/db");
+const Sequelize = require("sequelize");
+const FlightCard = require("./server/db/models/flightCard");
+const LodgingCard = require("./server/db/models/lodgingCard");
+const Trip = require("./server/db/models/trip");
