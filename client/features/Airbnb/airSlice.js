0c2d9b8624eafdff6877466e3e521e10ccadd7d5
veronica cisneors <vcisneroshdz@gmail.com>
2023-05-16 08:46:04 -0700

    carousel not working went back to using state

--- client/features/Airbnb/airSlice.js
+++ client/features/Airbnb/airSlice.js
@@ -33,7 +33,7 @@ export const fetchAirbnbListings = createAsyncThunk(
 
 const airSlice = createSlice({
   name: "airbnb",
-  initialState: { results: [], currentIndex: 0 },
+  initialState: [{ results: [], currentIndex: 0 }],
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchAirbnbListings.fulfilled, (state, action) => {
