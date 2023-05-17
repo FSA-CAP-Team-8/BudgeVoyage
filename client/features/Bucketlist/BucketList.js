48a0c76558593bdbc7f955432e7ce5be1e94ff74
jesiah <jesiah.roe@gmail.com>
2023-05-17 15:02:11 -0400

    Trip div and Lodging Div created

--- client/features/Bucketlist/BucketList.js
+++ client/features/Bucketlist/BucketList.js
@@ -5,9 +5,17 @@ const bucketlist = () => {
   return (
     <div id="bucketlist">
       <div className="bucketcontainer">
-        <h2>fights</h2>
-        <h2>lodging</h2>
-        <h2>total cost: 875$</h2>
+        <div className="Tripcontainer">
+          <span className="LodgingCard">
+            <img
+              className="FlightCardImage"
+              src="AbnbResult.webp"
+              alt="Airbnb-Listing-img"
+            ></img>
+            <div className="LodgingCardAmtDays">June 10 - June 12</div>
+            <div className="LodgingCardPriceTotal">$500 total</div>
+          </span>
+        </div>
       </div>
     </div>
   );
