830fe402391ad433286ba7194d893cefd9b36a7e
jesiah <jesiah.roe@gmail.com>
2023-05-17 15:07:50 -0400

    added dummy Flight Card

--- client/features/Bucketlist/BucketList.js
+++ client/features/Bucketlist/BucketList.js
@@ -8,12 +8,21 @@ const bucketlist = () => {
         <div className="Tripcontainer">
           <span className="LodgingCard">
             <img
-              className="FlightCardImage"
+              className="LodgingCardImage"
               src="AbnbResult.webp"
               alt="Airbnb-Listing-img"
             ></img>
             <div className="LodgingCardAmtDays">June 10 - June 12</div>
             <div className="LodgingCardPriceTotal">$500 total</div>
+          </span>
+          <span className="FlightCard">
+            <img
+              className="FlightCardImage"
+              src="FlightResult.jpg"
+              alt="Flight-Listing-img"
+            ></img>
+            <div className="FlightCardDates">June 10 - June 12</div>
+            <div className="FlightCardPriceTotal">$458 total</div>
           </span>
         </div>
       </div>
